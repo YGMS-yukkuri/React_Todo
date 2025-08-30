b7dadbc48fddbb54ca959b2517f185b18b22fb5e
yukkuri <reityan.sakura0104@gmail.com>
2025-08-30 10:11:47 +0900

    タイトルの作成

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Title.module.css'
+
+export default function Title() {
+    return (
+        <div className={styles.Title}>
+            <h1>Todo</h1>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Title from '../components/Title/Title'
 
 export default function Home() {
   return (
-    <div>Home</div>
+      <Title />
   )
 }
