d2a1eecc67c9e6006dd66fe8571f00ca29f90860
yukkuri <reityan.sakura0104@gmail.com>
2025-08-30 09:44:10 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Add from "./pages/Add"
+import Delete from "./pages/Delete"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/delete" element={<Delete />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
